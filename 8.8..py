4a029a7338e9110fdfd5540734a951ff0c1dbb1a
Nguyen-Van-Huy <60721751+Nguyen-Van-Huy@users.noreply.github.com>
2020-05-26 21:58:41 +0700

    Add files via upload

---
+++ 8.8..py
@@ -0,0 +1,41 @@
+from tkinter import *
+tk=Tk()
+tk.title('welcome')
+tk.geometry('350x350')
+lb1=Label(tk,text='Nguyen Van Huy',font=('time',20))
+lb1.place(x=100,y=50)
+lb2=Label(tk,text='msv:18575103010027',font=('time',20))
+lb2.place(x=100,y=100)
+lb3=Label(tk,text='date:23/08/2000',font=('time',20))
+lb3.place(x=100,y=150)
+tk.mainloop()
+#bai 8.8 b:
+from tkinter import *
+tk=Tk()
+tk.title('welcome')
+tk.geometry('350x350')
+var1=BooleanVar()
+var1.set(False)
+cb1=Checkbutton(tk,text='first',variable=var1)
+cb1.place(x=0,y=0)
+var2=BooleanVar()
+var2.set(False)
+cb2=Checkbutton(tk,text='second',variable=var2)
+cb2.place(x=0,y=50)
+var3=BooleanVar()
+var3.set(False)
+cb3=Checkbutton(tk,text='third',variable=var3)
+cb3.place(x=0,y=100)
+def click():
+    if var1.get()== True:
+        lb1=Label(tk,text='1')
+        lb1.place(x=200,y=0)
+    if var2.get()== True:
+        lb2=Label(tk,text='2')
+        lb2.place(x=200,y=50)
+    if var3.get()== True:
+        lb3=Label(tk,text='3')
+        lb3.place(x=200,y=100)
+bt=Button(tk,text='click me',command=click)
+bt.place(x=100,y=50)
+tk.mainloop()
